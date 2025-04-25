98d19b812a2d09b4eba94518d1f1b27bb43d550c
ezraeffect <build@fx0.kr>
2025-04-25 10:01:52 +0900

    초기 커밋.

---
+++ day5_1/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day5_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //MessageBox.Show("Hello, World!");
+
+            textBox_print.Text = "안녕하세요\r\n반갑습니다";
+        }
+    }
+}
